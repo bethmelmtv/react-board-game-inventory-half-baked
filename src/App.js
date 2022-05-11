c209d7b4e8a9d6e868c59053cbb5057342babd6f
Beth <bethmelmtv@gmail.com>
2022-05-11 14:42:26 -0700

    worked on detailpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import { useState, useEffect } from 'react';
 import { getUser } from './services/fetch-utils';
-import {
-  BrowserRouter as Router,
-  Switch,
-  NavLink,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, NavLink, Route, Redirect } from 'react-router-dom';
 import AuthPage from './AuthPage';
 import DetailPage from './DetailPage';
 import ListPage from './ListPage';
@@ -17,17 +11,29 @@ import { logout } from './services/fetch-utils';
 
 export default function App() {
   // You'll need to track the user in state
+  const [email, setEmail] = useState();
+  const [token, setToken] = useState();
 
   // add a useEffect to get the user and inject the user object into state on load
 
-  async function handleLogout() {
-    // call the logout function
-    // clear the user in state
-  }
+  useEffect(() => {
+    const user = getUser();
+    console.log(user);
+
+    if (user) {
+      setToken(user.access_token);
+      setEmail(user.user.email);
+    }
+  }, []);
+
+  // async function handleLogout() {
+  //   // call the logout function
+  //   // clear the user in state
+  // }
 
   return (
     <Router>
-      <div className='App'>
+      <div className="App">
         <header>
           {/* if there is a user in state, render out a link to the board games list, the create page, and add a button to let the user logout */}
         </header>
